a69aea75f4c5ba8c641b4fec15aee36169a19fcb
YunmiSeo <ymseo1014@gmail.com>
2020-11-18 17:45:45 +0900

    change header background-color on scrolling

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,24 @@
 const toggleBtn = document.querySelector(".toggle-menu-btn");
 const menuList = document.querySelector(".nav-menu-list");
+
+const navBar = document.querySelector(".page-header");
+
 const toggleMenu = () => {
   toggleBtn.classList.toggle("close");
   menuList.classList.toggle("show");
 };
 
+const showNav = () => {
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
+    navBar.classList.add("colored");
+  } else {
+    navBar.classList.remove("colored");
+  }
+};
+
 toggleBtn.addEventListener("click", toggleMenu);
+
+window.addEventListener("scroll", showNav);
