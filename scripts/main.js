26b1900769e553d9ab80f36f0c68fc39a2670663
YunmiSeo <ymseo1014@gmail.com>
2020-11-18 17:36:41 +0900

    make navbar.js

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector(".toggle-menu-btn");
-const menuList = document.querySelector(".nav-menu-list");
-const toggleMenu = () => {
-  toggleBtn.classList.toggle("close");
-  menuList.classList.toggle("show");
-};
-
-toggleBtn.addEventListener("click", toggleMenu);
