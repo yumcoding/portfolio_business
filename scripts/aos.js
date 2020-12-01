de86bc76ad8d4fbd5dde0522761ccb119400671a
YunmiSeo <ymseo1014@gmail.com>
2020-12-01 14:48:16 +0900

    add slide-in animation to section tags

---
+++ scripts/aos.js
@@ -0,0 +1,27 @@
+const sectionTags = document.querySelectorAll(".section-tag");
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () => {
+  sectionTags.forEach((tag) => {
+    if (isInViewport(tag)) {
+      tag.classList.add("slide-in-left");
+    } else {
+      tag.classList.remove("slide-in-left");
+    }
+  });
+};
+
+// Event Listeners
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
