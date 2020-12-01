95280da68d4bae2a42e56a2bfd1fbcaddf5f4c0b
YunmiSeo <ymseo1014@gmail.com>
2020-12-01 11:20:51 +0900

    add smooth scrolling when clicking a nav-bar menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,11 +3,13 @@ const menuList = document.querySelector(".nav-menu-list");
 
 const navBar = document.querySelector(".page-header");
 
+//toggle menu (mobile)
 const toggleMenu = () => {
   toggleBtn.classList.toggle("close");
   menuList.classList.toggle("show");
 };
 
+// show nav on scroll
 const showNav = () => {
   if (
     document.body.scrollTop >= 200 ||
@@ -19,6 +21,21 @@ const showNav = () => {
   }
 };
 
-toggleBtn.addEventListener("click", toggleMenu);
+//smooth scrolling when clicking a nav menu
+const smoothScrolling = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const href = e.target.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
 
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  }
+};
+
+//Event Listeners
+toggleBtn.addEventListener("click", toggleMenu);
 window.addEventListener("scroll", showNav);
+navBar.addEventListener("click", smoothScrolling);
